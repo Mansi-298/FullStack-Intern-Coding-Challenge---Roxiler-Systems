7962a44c621f71ab561bbf8809718615b7915d53
Mansi-298 <waghmansi298@gmail.com>
2025-06-03 19:21:51 +0530

    app.js added

--- server/app.js
+++ server/app.js
@@ -17,4 +17,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const sequelize = require('./models/index');
+const User = require('./models/User');
+const Store = require('./models/Store');
+const Rating = require('./models/Rating');
+
+// Define associations
+User.hasMany(Rating, { foreignKey: 'user_id' });
+Store.hasMany(Rating, { foreignKey: 'store_id' });
+Store.belongsTo(User, { foreignKey: 'owner_id', as: 'owner' });
+Rating.belongsTo(User, { foreignKey: 'user_id' });
+Rating.belongsTo(Store, { foreignKey: 'store_id' });
+
+// Sync models (for dev only, use migrations in prod)
+if (process.env.NODE_ENV !== 'production') {
+  sequelize.sync();
+}
+
 module.exports = app;
